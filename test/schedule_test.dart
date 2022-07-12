2b4418e49a7da1822fa3c0a02196a02970a4deaa
clrayel <clrayel@up.edu.ph>
2022-07-12 13:58:01 +0800

    Created a test file for schedule

---
+++ test/schedule_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ui_collection/modules/schedule/domain/entities/schedule_item_entity.dart';
+
+void main(List<String> args) {
+  test('Schedule entity should be of type ScheduleItemEntity', () {
+    final ScheduleItemEntity schedule = ScheduleItemEntity(
+        dayOfWeek: 1,
+        status: "Regular Shift",
+        shiftStart: "9:00 AM",
+        shiftEnd: "6:00 PM",
+        dateTime: DateTime(2022, 7, 4));
+
+    expect(schedule.dayOfWeek, 1);
+  });
+}
